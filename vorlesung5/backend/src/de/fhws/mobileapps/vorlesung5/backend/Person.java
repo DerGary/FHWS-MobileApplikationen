4fecfa8f863e502839eb1b2581dc8b762e97dfc4
braunpet <mail@peter-braun.org>
2014-04-13 20:01:20 +0200

    Added pagination, refactoring

--- vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/Person.java
+++ vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/Person.java
@@ -1,51 +1,60 @@
 package de.fhws.mobileapps.vorlesung5.backend;
 
-public class Person {
+public class Person
+{
 
 	private long id;
-	
+
 	private String firstName;
-	
+
 	private String lastName;
-	
+
 	private String url;
 
-	
-	public Person(String firstName, String lastName) {
-		super();
+	public Person( )
+	{}
+
+	public Person( String firstName, String lastName )
+	{
+		super( );
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.url = "http://lorempixel.com/300/300/people/";
 	}
 
-	public long getId() {
+	public long getId( )
+	{
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId( long id )
+	{
 		this.id = id;
 	}
 
-	public String getFirstName() {
+	public String getFirstName( )
+	{
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setFirstName( String firstName )
+	{
 		this.firstName = firstName;
 	}
 
-	public String getLastName() {
+	public String getLastName( )
+	{
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
+	public void setLastName( String lastName )
+	{
 		this.lastName = lastName;
 	}
-	
-	public String toString()
+
+	public String toString( )
 	{
-		return Long.toString(this.id) + ";" + this.firstName + ";" + this.lastName + ";" + this.url;
+		return Long.toString( this.id ) + ";" + this.firstName + ";" + this.lastName + ";" + this.url;
 	}
-	
 
 }
--- vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/Resource.java
+++ vorlesung5/backend/src/de/fhws/mobileapps/vorlesung5/backend/Resource.java
@@ -1,9 +1,9 @@
 package de.fhws.mobileapps.vorlesung5.backend;
 
-import java.util.Collection;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -11,37 +11,44 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
-
-@Path("persons")
-public class Resource 
+@Path( "persons" )
+public class Resource
 {
+	private final static String SIZE_HEADER = "X-persons-length";
+
 	@GET
-	@Produces( "application/json" )
-	public Collection<Person> printHelloWorld( )
+	@Produces( MediaType.APPLICATION_JSON )
+	public Response loadPersons(
+		@DefaultValue( "2147483647" ) @QueryParam( "size" ) int size,
+		@DefaultValue( "0" ) @QueryParam( "offset" ) int offset )
 	{
-		return PersonContainer.getInstance().values();
+		final List<Person> result = new ArrayList<Person>( PersonContainer.getInstance( ).values( ) );
+		final int sizeOfResult = result.size( );
+		final int toIndex = Math.min( sizeOfResult, offset + size );
+		return Response.ok( result.subList( offset, toIndex ) ).header( SIZE_HEADER, sizeOfResult ).build( );
 	}
-	
+
 	@GET
 	@Path( "/{personid}" )
-	@Produces( "application/json" )
+	@Produces( MediaType.APPLICATION_JSON )
 	public Person loadPerson( @DefaultValue( "-1" ) @PathParam( "personid" ) long personId )
 	{
-		return PersonContainer.getInstance().load(personId);
-	}
-	
-	@POST
-	@Consumes( "application/json" )
-	public Response createPerson( Person person, @Context HttpServletRequest httpRequest )
-	{
-		long id = PersonContainer.getInstance().save(person);
-		String url = httpRequest.getRequestURL().toString();
-		
-		return Response.status(HttpServletResponse.SC_CREATED).header("Location", url + "/" + id ).build();
+		return PersonContainer.getInstance( ).load( personId );
 	}
 
-	
+	@POST
+	@Consumes( MediaType.APPLICATION_JSON )
+	public Response createPerson( Person person, @Context UriInfo uriInfo )
+	{
+		final long id = PersonContainer.getInstance( ).save( person );
+		final URI loc = uriInfo.getAbsolutePathBuilder( ).path( Long.toString( id ) ).build( );
+		return Response.created( loc ).build( );
+	}
+
 }
